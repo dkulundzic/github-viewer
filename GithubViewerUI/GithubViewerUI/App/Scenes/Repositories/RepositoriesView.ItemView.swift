9ff80e03218f0918afa2e751fdfb36dfd1079cbc
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2023-02-13 11:40:18 +0100

    Added the ItemView skeleton

---
+++ GithubViewerUI/GithubViewerUI/App/Scenes/Repositories/RepositoriesView.ItemView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension RepositoriesView {
+  struct ItemView: View {
+    var body: some View {
+      Text("Hello, World!")
+    }
+  }
+}
+
+struct ItemView_Previews: PreviewProvider {
+  static var previews: some View {
+    RepositoriesView.ItemView()
+  }
+}
