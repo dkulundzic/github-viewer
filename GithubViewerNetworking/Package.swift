9b4988a8af723038316e3372b1f91b47c29a5c85
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2023-02-13 11:37:48 +0100

    Project configuration updates

--- GithubViewerNetworking/Package.swift
+++ GithubViewerNetworking/Package.swift
@@ -15,11 +15,9 @@ let package = Package(
     .package(path: "../GithubViewerModel")
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "GithubViewerNetworking",
-      dependencies: []),
+      dependencies: ["GithubViewerModel"]),
     .testTarget(
       name: "GithubViewerNetworkingTests",
       dependencies: ["GithubViewerNetworking"]),
